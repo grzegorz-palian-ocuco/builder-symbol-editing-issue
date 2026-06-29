3b36ea762bbff255282cef66a98198208162be5e
Grzegorz Palian <grzegorz.palian@ocuco.com>
2026-06-29 15:19:12 +0200

    Update dependencies and refactor tmp-page component for improved data handling

--- src/pages/tmp-page.tsx
+++ src/pages/tmp-page.tsx
@@ -4,27 +4,7 @@ export const getStaticProps = async () => {
   const pageContent = await builder
     .get("page", {
       userAttributes: {
-        urlPath: "/tmp-page",
-        customer: "ocuco",
-      },
-      cachebust: true,
-    })
-    .toPromise();
-
-  const headerContent = await builder
-    .get("header", {
-      userAttributes: {
-        urlPath: "/tmp-page",
-        customer: "ocuco",
-      },
-      cachebust: true,
-    })
-    .toPromise();
-
-  const footerContent = await builder
-    .get("footer", {
-      userAttributes: {
-        urlPath: "/tmp-page",
+        urlPath: "/tmp-cross-origin-error",
         customer: "ocuco",
       },
       cachebust: true,
@@ -32,36 +12,17 @@ export const getStaticProps = async () => {
     .toPromise();
 
   return {
-    props: { pageContent, headerContent, footerContent },
+    props: { pageContent },
     revalidate: 5,
   };
 };
 
-const exampleData1 = {
-  name: "Product Foo",
-  price: 100,
-  isAvailable: true,
-};
-
-const exampleData2 = {
-  name: "Product Bar",
-  price: 200,
-  isAvailable: false,
-};
-
-const Page = ({ pageContent, headerContent, footerContent }: any) => (
+const Page = ({ pageContent }: any) => (
   <>
-    <BuilderComponent model="header" content={headerContent} />
     <BuilderComponent
       model="page"
       content={pageContent}
-      data={{
-        exampleProduct: Math.random() > 0.5 ? exampleData1 : exampleData2,
-        exampleEssentialData:
-          "This data is essential for the symbol to display correctly",
-      }}
     />
-    <BuilderComponent model="footer" content={footerContent} />
   </>
 );
 
